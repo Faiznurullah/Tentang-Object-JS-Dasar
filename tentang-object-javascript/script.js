91b14545e448fd16c38bc8f2924be5d51679872b
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2022-07-04 08:25:47 +0700

    add folder and file

---
+++ tentang-object-javascript/script.js
@@ -0,0 +1,18 @@
+
+ // Object Orang
+ const Orang = {
+
+    nama : 'Herlambang',
+    umur : 22,
+    pekerjaan : 'Programmer',
+    skill : ['html', 'css', 'javascript'],
+    alamat : {
+
+         kota : 'Bogor',
+         provinsi : 'Jawa Barat',
+         negara : 'Indonesia'
+
+    }
+
+
+ }
